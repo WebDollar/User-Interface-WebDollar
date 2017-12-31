416cc1e390122e1c90ebbf10b790be8869c98dcc
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-12-31 15:47:13 +0200

    Network Maps

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+import NetworkNativeMap from "maps/Native-Map/Network-Native-Map"
+import NetworkGoogleMaps from "maps/Google-Maps/Network-Google-Maps.js"
+
 console.log("hello world");
+
+let exportObject = {
+    Network:{
+        NativeMap: NetworkNativeMap,
+        GoogleMaps: NetworkGoogleMaps,
+    }
+};
+
+
+module.exports =  exportObject;
+
+//browser minimized script
+if ( typeof global.window !== 'undefined')
+    global.window.WebDollar = exportObject;
+
+if ( typeof window !== 'undefined')
+    window.WebDollar = exportObject;

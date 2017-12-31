616bf536830198b7f597b92e059a7dbd247c3cbe
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-12-31 18:28:55 +0200

    dom

--- src/maps/Native-Map/Network-Native-Map-DOM.js
+++ src/maps/Native-Map/Network-Native-Map-DOM.js
@@ -1,6 +1,21 @@
-//var networkCSS = require('./res/network-native.css');
+var networkCSS = require('./res/network-native.css');
 var networkMapHtml = require('./res/network-native-map.html');
 var networkMapDialogHtml = require('./res/network-native-map-dialog.text');
+
+console.log("networkCSS",networkCSS)
+console.log("networkMapHtml",networkMapHtml)
+console.log("networkMapDialogHtml",networkMapDialogHtml)
+
+// var fs = require('browserify-fs');
+
+import FileHelpers from "helpers/File.helpers"
+
+// FileHelpers.readFileSync(__direc 'res/network-native-map.html', 'utf8').then((answer)=>{
+//     console.log("aaasnwer",answer)
+// })
+//
+// var networkMapHtml = FileHelpers.readFileSync('res/network-native-map.html', 'utf8');
+// var networkMapDialogHtml = FileHelpers.readFileSync('res/network-native-map-dialog.text');
 
 class NetworkNativeMapDOM{
 
@@ -19,7 +34,9 @@ class NetworkNativeMapDOM{
 
         let parent = document.getElementById(divId);
         
-        console.log("networkMapDialogHtml",networkMapDialogHtml);;
+        console.log("networkMapDialogHtml",networkMapDialogHtml);
+        console.log("networkMapHtml",networkMapHtml);
+        console.log("networkMapDialogHtml",networkMapDialogHtml);
 
         if (document.getElementsByClassName('map-dialog').length === 0)
             parent.insertAdjacentHTML( 'beforebegin', networkMapDialogHtml.toString() );

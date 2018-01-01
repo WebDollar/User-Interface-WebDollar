65e7320b4f53c62f7d45cbb54b262c6642582bbc
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-01 17:59:09 +0200

    icon changes

--- src/wallet/Wallet-DOM.js
+++ src/wallet/Wallet-DOM.js
@@ -1,5 +1,5 @@
 import AddressDOM from './address/Address-DOM';
-import FontsDOM from "UI/fonts/Fonts-DOM"
+
 
 var CSSId = "css"+Math.floor(Math.random()*100000);
 var CSS = "<style id='"+CSSId+"'>"+require('./res/wallet.css')+"</style>";
@@ -11,48 +11,6 @@ class WalletDOM{
 
 
     }
-
-    addCSS(type){
-
-        if (document.getElementById(CSSId) !== null) return false;
-
-        let parent = document.getElementsByTagName("head")[0];
-
-        parent.insertAdjacentHTML( 'beforeend',CSS);
-
-        AddressDOM.addCSS(type);
-    }
-
-    addHTML(divId){
-
-        let parent = document.getElementById(divId);
-
-        let html = Html;
-
-        console.log("parent", parent);
-
-        if (parent === null) {
-
-            parent = document.getElementsByTagName("body")[0];
-
-            parent.insertAdjacentHTML( 'beforeend',`<div id="${divId}"> </div>`);
-            parent = document.getElementById(divId);
-
-        } else{
-
-        }
-
-        parent.insertAdjacentHTML( 'beforeend', html);
-
-        //add icons
-        parent = document.querySelector("#dashboardWallet .walletController");
-
-        FontsDOM.addHTML(parent, "plus", "walletControllerPlus");
-        FontsDOM.addHTML(parent, "lock-open", "walletControllerLock");
-
-    }
-
-
 
     /*
         It will refresh the wallet list by adding new elements

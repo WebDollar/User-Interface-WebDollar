25de7c4fc52df804999a088464b93160da9671c9
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-01 17:42:38 +0200

    Icons

---
+++ src/components/UI/icons/icon.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <div>
+
+        <svgKey v-if="this.icon === 'key'"></svgKey>
+        <svgLockClosed v-if="this.icon === 'lock-closed'"></svgLockClosed>
+        <svgLockOpen v-if="this.icon === 'lock-open'"></svgLockOpen>
+        <svgPlus v-if="this.icon === 'plus'"></svgPlus>
+        <svgPlusSquare v-if="this.icon === 'plus-square'"></svgPlusSquare>
+        <svgX v-if="this.icon === 'x'"></svgX>
+
+    </div>
+
+</template>
+
+
+<script>
+
+    import svgKey from "./res/svg-key.vue"
+    import svgLockClosed from "./res/svg-lock-closed.vue"
+    import svgLockOpen from "./res/svg-lock-open.vue"
+    import svgPlus from "./res/svg-plus.vue"
+    import svgPlusSquare from "./res/svg-plus-square.vue"
+    import svgX from "./res/svg-x.vue"
+
+    export default{
+
+        props:{
+            icon: {default: ''}
+        },
+
+        components:{
+            "svgKey": svgKey,
+            "svgLockClosed": svgLockClosed,
+            "svgLockOpen": svgLockOpen,
+            "svgPlus": svgPlus,
+            "svgPlusSquare": svgPlusSquare,
+            "svgX": svgX,
+        }
+
+    }
+</script>
+
+<style>
+
+</style>

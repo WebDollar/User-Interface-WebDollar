6c808da1a68684988c089dd9fac6f6b996bb299a
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-02 00:00:30 +0200

    Global Initiazation

---
+++ src/initialize-params/global-initialize/Global-Initialization.js
@@ -0,0 +1,37 @@
+import BrowserHelpers from "helpers/Browser.helpers"
+
+class GlobalInitialization{
+
+    constructor(){
+
+    }
+
+
+    initializeGlobalSettings(){
+
+        //-----------------------
+        // Int Script
+        //-----------------------
+
+        if (document.getElementById("WebdollarFont") === null)
+            document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend",`<link id="WebdollarFont" href="http://192.168.1.2:8080/public/assets/fonts/avenir-light.woff" rel="stylesheet">`);
+
+        if (document.getElementById("WebdollarViewPort") === null)
+            document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<meta id="WebdollarViewPort" name="viewport" content="width=device-width, initial-scale=1.0"/>`)
+
+        if (document.getElementById("WebDollarModalBackground") === null)
+            document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<div id="WebDollarModalBackground" > </div>`);
+
+        window.screenHeight = window.innerHeight;
+        window.screenWidth = window.innerWidth;
+
+        BrowserHelpers.addEvent(window, "resize", (event) => {
+            window.screenHeight = window.innerHeight;
+            window.screenWidth = window.innerWidth;
+        });
+    }
+
+
+}
+
+export default new GlobalInitialization()

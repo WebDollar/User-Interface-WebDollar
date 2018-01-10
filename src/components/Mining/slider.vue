d88a3a794e6fb8a1f9036fc1e1bf7a7ac3144dca
Stroe Silviu Bogdan <silviu@silviu-s.com>
2018-01-10 14:08:46 +0200

    fixed mining bar threads on safari

--- src/components/Mining/slider.vue
+++ src/components/Mining/slider.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <vue-slider class="miningSlider" @callback="this.change" :piecewise="true" :width="this.screenWidth < 750 ? 180 : 330" :tooltip="false" :min="0" :max="this.logicalProcessors" v-model="value"></vue-slider>
+        <vue-slider class="miningSlider" @callback="this.change" :piecewise="true"
+                    :width="this.screenWidth < 750 ? 180 : 330" :tooltip="false" :min="0" :max="this.logicalProcessors"
+                    v-model="value"></vue-slider>
     </div>
 </template>
 <script>
@@ -11,10 +13,10 @@
         name: 'slider',
 
         components: {
-            "vueSlider":vueSlider
+            "vueSlider": vueSlider
         },
 
-        data () {
+        data() {
             return {
                 value: 0,
                 screenWidth: window.innerWidth
@@ -22,32 +24,32 @@
         },
 
         methods: {
-            change (value) {
+            change(value) {
                 this.$emit('sliderChanged', value)
             },
-            addEvent (object, type, callback) {
+            addEvent(object, type, callback) {
                 if (object === null || typeof(object) === 'undefined') return;
                 if (object.addEventListener) {
                     object.addEventListener(type, callback, false);
                 } else if (object.attachEvent) {
                     object.attachEvent("on" + type, callback);
                 } else {
-                    object["on"+type] = callback;
+                    object["on" + type] = callback;
                 }
             },
         },
 
-        mounted(){
+        mounted() {
 
             this.addEvent(window, "resize", (event) => {
 
-               this.screenWidth = window.innerWidth;
+                this.screenWidth = window.innerWidth;
 
             });
 
             this.screenWidth = window.innerWidth;
 
-            this.logicalProcessors = 2 * window.navigator.hardwareConcurrency;
+            this.logicalProcessors = window.navigator.hardwareConcurrency === undefined ? 8 : 2 * window.navigator.hardwareConcurrency;
 
         }
     }
@@ -55,24 +57,24 @@
 
 <style>
 
-    .miningSlider{
-        padding-top: 15px!important;
-        padding-bottom: 15px!important;
-        padding-left: 20px!important;
+    .miningSlider {
+        padding-top: 15px !important;
+        padding-bottom: 15px !important;
+        padding-left: 20px !important;
         background-color: #262626;
     }
 
-    .vue-slider-component .vue-slider-piecewise{
-        background-color: #424242!important;
+    .vue-slider-component .vue-slider-piecewise {
+        background-color: #424242 !important;
     }
 
-    .vue-slider-component .vue-slider-process{
+    .vue-slider-component .vue-slider-process {
         /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#fec02c+29,bc0505+100 */
-        background: #fec02c!important; /* Old browsers */
-        background: -moz-linear-gradient(left, #fec02c 29%, #bc0505 100%)!important; /* FF3.6-15 */
-        background: -webkit-linear-gradient(left, #fec02c 29%,#bc0505 100%)!important; /* Chrome10-25,Safari5.1-6 */
-        background: linear-gradient(to right, #fec02c 29%,#bc0505 100%)!important; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fec02c', endColorstr='#bc0505',GradientType=1 )!important; /* IE6-9 */
+        background: #fec02c !important; /* Old browsers */
+        background: -moz-linear-gradient(left, #fec02c 29%, #bc0505 100%) !important; /* FF3.6-15 */
+        background: -webkit-linear-gradient(left, #fec02c 29%, #bc0505 100%) !important; /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(to right, #fec02c 29%, #bc0505 100%) !important; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#fec02c', endColorstr='#bc0505', GradientType=1) !important; /* IE6-9 */
     }
 
 </style>

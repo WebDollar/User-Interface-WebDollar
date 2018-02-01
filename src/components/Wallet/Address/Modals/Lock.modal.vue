ec57bdcf7705d36897011ba3351149af3e0aed56
alexandru.panait <ionutalexpanait@gmail.com>
2018-02-01 22:38:59 +0200

    wallet secure structure

--- src/components/Wallet/Address/Modals/Lock.modal.vue
+++ src/components/Wallet/Address/Modals/Lock.modal.vue
@@ -2,11 +2,11 @@
 
     <div>
 
-        <Modal title="Delete Adress" ref="refModal">
+        <Modal title="Wallet Address Secure" ref="refModal">
 
             <div slot="content">
 
-                <div class="descriptionText">
+                <div class="descriptionTextPass">
 
                     Set your password
 
@@ -14,10 +14,19 @@
 
                 <div >
 
-                    <input class="inputDeleteModal"/>
+                    <div class="inputAndGeneratorPass">
+                        <div>
+                            <input v-Model="this.walletAddressPassword" class="inputDeleteModalPass"/>
+                        </div>
+                        <div>
+                            <div @click="" class="modalButtonPass generatorButtonPass">
+                                Generate
+                            </div>
+                        </div>
+                    </div>
 
-                    <div @click="" class="modalButton">
-                        Save
+                    <div @click="this.encryptPassword" class="modalButtonPass">
+                        Set Password
                     </div>
 
                 </div>
@@ -31,17 +40,17 @@
 </template>
 
 <script>
+
     var Vue = require('vue/dist/vue.min.js');
-    import Modal from "components/UI/modal/Modal.vue"
+    import Modal from "components/UI/modal/Modal.vue";
 
-    import Clipboard from './../../../../../node_modules/v-clipboard'
+    import Clipboard from './../../../../../node_modules/v-clipboard';
 
-    Vue.use(Clipboard)
+    Vue.use(Clipboard);
 
     export default {
 
         props: {
-
             address: {default: null},
             toAddress: {default: null},
             toAmount: {default: 0.0},
@@ -53,64 +62,42 @@
 
         data: () => {
             return {
-                isTransfer: false,
-                isSell: false,
-                isBuy: false,
-                isTransactionList: false,
                 clipboardText: 'Copy to Clipboard',
+                walletAddressPassword: ''
             }
         },
 
         methods: {
 
-            showTransfer() {
-                this.isTransfer = true;
-                this.isSell = false;
-                this.isBuy = false;
-                this.isTransactionList=false;
-            },
-            showBuy() {
-                this.isTransfer = false;
-                this.isSell = false;
-                this.isBuy = true;
-                this.isTransactionList = false;
-            },
-            showSell() {
-                this.isTransfer = false;
-                this.isSell = true;
-                this.isBuy = false;
-                this.isTransactionList = false;
-            },
-            showTransactions(){
-                this.isTransfer = false;
-                this.isSell = false;
-                this.isBuy = false;
-                this.isTransactionList = true;
-            },
             closeModal() {
                 this.$refs['refModal'].closeModal();
             },
 
             showModal(e) {
 
-                console.log("teeest showModal", this.$refs['refModal']);
-                console.log("teeest showModal", this.$refs['refModal'].modalOpened);
-
                 if (this.$refs['refModal'].modalOpened === false){
                     console.log("shooow modal");
                     this.$refs['refModal'].showModal();
                 }
+
             },
             copyToClipboard(){
+
                 this.clipboardText = 'Copied';
                 this.$clipboard(this.address)
+
+            },
+
+            encryptPassword(){
+
+                var wordsArray = this.walletAddressPassword.split(' ');
+                console.log(this.walletAddressPassword);
+
             }
 
         },
 
         mounted() {
-
-            this.clipboardText = 'Copy to Clipboard';
 
             if (typeof window === 'undefined') return;
 
@@ -121,48 +108,66 @@
 </script>
 
 <style>
-    .descriptionText{
-
+    .descriptionTextPass{
         color: #bdbdbd;
         padding: 30px 10px;
-
     }
 
-    .inputDeleteModal{
-        width: 90%;
+    .inputDeleteModalPass{
+        width: 100%;
+        width: -webkit-fill-available;
         background-color: #2d2d2d;
-        border: solid 1px #565656;
+        border: none;
         margin: 0 auto;
         left: 0;
         right: 0;
         color: #ccc;
-        padding: 7px;
+        padding: 7px 0;
         font-weight: 100;
-        text-align: center;
         font-size: 14px;
-        margin-bottom: 20px;
+        padding-left: 5px;
     }
 
-    .modalButton{
+    .modalButtonPass{
         background-color: #131313;
         color: #969696;
         font-size: 12px;
         width: 140px;
-        border: solid 1px #5f5d5d;
         font-weight: bolder;
         border-radius: 5px;
         letter-spacing: 2px;
         text-transform: uppercase;
-        padding: 8px;
+        padding: 8px 20px;
         margin: 0 auto;
+        cursor: pointer;
+        border: solid 1px #5f5d5d;
         margin-bottom: 20px;
         transition: all .3s linear;
     }
 
-    .modalButton:hover{
+    .modalButtonPass:hover{
         background-color: #f6ba2c;
         color: #000000;
         transition: all .3s linear;
     }
+
+    .inputAndGeneratorPass{
+        width: 90%;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        border: solid 1px #5f5d5d;
+        margin-bottom: 20px;
+    }
+
+    .generatorButtonPass{
+        margin: 0!important;
+        width: 100%!important;
+        border-radius: 0!important;
+        border: none;
+        height: 15px!important;
+        padding: 8px 0!important;
+    }
+
 </style>
 

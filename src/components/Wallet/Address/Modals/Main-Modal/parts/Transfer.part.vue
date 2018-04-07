3e44595a596b30d5382e17f9da29a1ceb3e8407c
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-04-08 00:53:38 +0300

    max 4 digits

--- src/components/Wallet/Address/Modals/Main-Modal/parts/Transfer.part.vue
+++ src/components/Wallet/Address/Modals/Main-Modal/parts/Transfer.part.vue
@@ -133,7 +133,24 @@
 
             },
 
+            decimalPlaces(num) {
+                var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+                if (!match) { return 0; }
+                return Math.max(
+                    0,
+                    // Number of digits right of decimal point.
+                    (match[1] ? match[1].length : 0)
+                    // Adjust for scientific notation.
+                    - (match[2] ? +match[2] : 0));
+            },
+
             handleChangeToAmount(e){
+
+                var digits = decimalPlaces(this.toAmount);
+
+                if (digits>4){
+                    this.toAmount = this.toAmount.toFixed(4);
+                }
 
                 this.errorToAmountMessage = '';
 

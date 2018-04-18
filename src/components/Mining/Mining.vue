a001d7947cdfacf2017cb2740294978c6f74120b
Cosmin-Dumitru Oprea <opreacosmind@gmail.com>
2018-04-18 11:28:50 +0300

    Little change
    Signed-off-by: Cosmin-Dumitru Oprea <opreacosmind@gmail.com>

--- src/components/Mining/Mining.vue
+++ src/components/Mining/Mining.vue
@@ -119,7 +119,7 @@
 
             WebDollar.StatusEvents.on("blockchain/status", (data)=>{
 
-               // if (data.message === "Blockchain Ready to Mine") {
+                if (data.message === "Blockchain Ready to Mine") {
 
                     this.loaded = true;
                     this.$refs['refMiningSlider'].disabled = false;
@@ -131,7 +131,7 @@
 
                     console.error('#################################################### s-a synchronizat');
 
-                //}
+                }
 
             });
 

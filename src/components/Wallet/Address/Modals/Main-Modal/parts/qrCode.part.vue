f40967cb2499f1fdbc22282335d8f915e7c5d4d4
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2019-05-24 17:22:20 +0300

    adding QrCode switcher

--- src/components/Wallet/Address/Modals/Main-Modal/parts/qrCode.part.vue
+++ src/components/Wallet/Address/Modals/Main-Modal/parts/qrCode.part.vue
@@ -2,7 +2,13 @@
 
     <div class="transferList qrCodeContainer" ref="qrCode">
 
-        <img class="qrCodeImage" src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&chld=|0&chl=http%3A%2F%2Fwww.the-qrcode-generator.com%2F"/>
+        <div class="extraInfo">
+             <span @click="changeQR" class="copyButton modalButton2 qrCodeButton">
+                 {{ this.qrType === 'link' ? 'Switch to WEBD Address QR Code' : 'Switch to direct payment link QR Code' }}
+             </span>
+        </div>
+
+        <img class="qrCodeImage" :src=" this.qrType === 'link' ? 'http://chart.apis.google.com/chart?chs=300x300&cht=qr&chld=|0&chl=http%3A%2F%2Fwww.the-qrcode-generator.com%2F' : 'https://cdn.shopify.com/growth-tools-assets/qr-code/shopify-faae7065b7b351d28495b345ed76096c03de28bac346deb1e85db632862fd0e4.png'"/>
 
     </div>
 
@@ -12,6 +18,25 @@
 
     export default {
 
+        data: () => {
+            return {
+                qrType: "link",
+            }
+        },
+
+
+        methods:{
+
+            changeQR(){
+
+                if (this.qrType === 'link')
+                    this.qrType = 'address';
+                else
+                    this.qrType = 'link';
+
+            }
+
+        }
 
     }
 </script>
@@ -32,5 +57,22 @@
         position: relative;
     }
 
+    .qrType{
+        color: #b6b6b6;
+        font-size: 18px;
+    }
+
+    .qrCodeContainer .extraInfo{
+        margin-bottom: 20px;
+    }
+
+    .qrCodeButton{
+        width: 100%;
+        display: block;
+        color: #cdcdcd;
+        font-size: 14px;
+        padding: 10px 0;
+        margin-top: 0;
+    }
 
 </style>

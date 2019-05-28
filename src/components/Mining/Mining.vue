7d9945382137fbfb7a1ec2e37150727e79818fbb
Lauren ten Hoor <laurentenhoor@gmail.com>
2019-05-28 03:08:32 +0200

    Fixed issue with loader showing without mining power

--- src/components/Mining/Mining.vue
+++ src/components/Mining/Mining.vue
@@ -14,7 +14,7 @@
                     <p class="miningTypeIndicator">
                         {{this.started ? ((this.hashesPerSecond <= 1) ? 'PoS Mining ' : 'PoW Mining ') : 'Not Started'}}
                     </p>
-                    <svg :style="{display: (this.hashesPerSecond > 0) ? 'inline-block' : 'none'}" version="1.1" class="miningLoader" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    <svg :style="{display: (this.started && this.hashesPerSecond > 0) ? 'inline-block' : 'none'}" version="1.1" class="miningLoader" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                      width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
                       <path fill="#fec02c" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
                         <animateTransform attributeType="xml"

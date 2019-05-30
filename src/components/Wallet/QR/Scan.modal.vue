a06c11765f306ec3eb84c387b36765ac584a09fb
Lauren ten Hoor <laurentenhoor@gmail.com>
2019-05-30 21:20:07 +0200

    Goto url after scanning

--- src/components/Wallet/QR/Scan.modal.vue
+++ src/components/Wallet/QR/Scan.modal.vue
@@ -33,6 +33,31 @@ export default {
         this.$refs["qrScanner"].showModal();
       }
     },
+
+    onDecode (content) {
+      window.location.href = content;
+    },
+
+    onInit (promise) {
+      promise.then(() => {
+        console.log('Successfully initilized! Ready for scanning now!')
+      })
+      .catch(error => {
+        if (error.name === 'NotAllowedError') {
+          this.errorMessage = 'Hey! I need access to your camera'
+        } else if (error.name === 'NotFoundError') {
+          this.errorMessage = 'Do you even have a camera on your device?'
+        } else if (error.name === 'NotSupportedError') {
+          this.errorMessage = 'Seems like this page is served in non-secure context (HTTPS, localhost or file://)'
+        } else if (error.name === 'NotReadableError') {
+          this.errorMessage = 'Couldn\'t access your camera. Is it already in use?'
+        } else if (error.name === 'OverconstrainedError') {
+          this.errorMessage = 'Constraints don\'t match any installed camera. Did you asked for the front camera although there is none?'
+        } else {
+          this.errorMessage = 'UNKNOWN ERROR: ' + error.message
+        }
+      })
+    }
   }
 
 };

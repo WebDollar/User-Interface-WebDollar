f03d014edc921b7f4c74958bc4f79c063a9164ae
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2019-06-05 13:47:07 +0300

    add a single qrCode

--- src/components/Wallet/Address/Modals/Main-Modal/parts/qrCode.part.vue
+++ src/components/Wallet/Address/Modals/Main-Modal/parts/qrCode.part.vue
@@ -2,13 +2,7 @@
 
     <div class="transferList qrCodeContainer" ref="qrCode">
 
-        <div class="extraInfo">
-             <span @click="changeQR" class="copyButton modalButton2 qrCodeButton">
-                 {{ this.qrType === 'link' ? 'Switch to WEBD Address QR Code' : 'Switch to direct payment link QR Code' }}
-             </span>
-        </div>
-
-        <img class="qrCodeImage" :src=" this.qrType === 'link' ? 'http://chart.apis.google.com/chart?chs=300x300&cht=qr&chld=|0&chl=http%3A%2F%2Fwww.the-qrcode-generator.com%2F' : 'https://cdn.shopify.com/growth-tools-assets/qr-code/shopify-faae7065b7b351d28495b345ed76096c03de28bac346deb1e85db632862fd0e4.png'"/>
+        <img class="qrCodeImage" src="https://cdn.shopify.com/growth-tools-assets/qr-code/shopify-faae7065b7b351d28495b345ed76096c03de28bac346deb1e85db632862fd0e4.png"/>
 
     </div>
 
@@ -19,22 +13,12 @@
     export default {
 
         data: () => {
-            return {
-                qrType: "link",
-            }
+
         },
 
 
         methods:{
 
-            changeQR(){
-
-                if (this.qrType === 'link')
-                    this.qrType = 'address';
-                else
-                    this.qrType = 'link';
-
-            }
 
         }
 
